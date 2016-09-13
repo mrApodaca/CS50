c3fe4fd39fc6407dfcd978ed7f403f8fa04abaeb
mrApodaca <mrApodaca@users.noreply.github.com>
2016-09-13 08:55:21 -0600

    Update loops.c

--- Raymond/loops.c
+++ Raymond/loops.c
@@ -1,1 +1,18 @@
-main(void);
+for (int i = 0; i < 10; i++)  //variable, integer named i, i is initially equal to 0, while i is less than 10, add one to i
+    {
+        printf("hello!\n");
+	}
+for (int j = 0; j < 10; j++)  //three parts initialization (j=0), condition (j<10), and modification (j++)
+    {
+        printf("%d\n", j);
+    }
+int k = 0; //initialization (k=0)
+while (k < 10) //condition (k<10)
+  {
+      printf("%d\n", k);
+      k++; //modification (k++)
+  }
+while (true) //this is always true, so it will run forever
+  {
+      printf("hello!\n");
+  }
