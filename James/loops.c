8f63479e3c080d2b380e1d2cd907e398fb7701e7
jamesatencio <jatencio@centennialschool.net>
2016-09-13 09:00:13 -0600

    Update loops.c

--- James/loops.c
+++ James/loops.c
@@ -1,1 +1,18 @@
-main void;
+	  for (int i = 0; i < 10; i++)  //variable, integer named i, i is initially equal to 0, while i is less than 10
+      {
+          printf("hello!\n");
+      }
+for (int j = 0; j < 10; j++)  //three parts initialization (j=0),condition (j<10),
+      {
+          printf("%d\n", j);
+      }
+int k = 0;
+while (k < 10)
+  {
+      printf("%d\n", k);
+      k++;
+  } 
+while (true)
+  {
+      printf("hello!\n");
+  }
