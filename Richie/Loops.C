5e02c02f8651282fc214bdd57d1f34100c8b4e94
RichieAtencio <RichieAtencio@users.noreply.github.com>
2016-09-13 08:58:26 -0600

    Create Loops.C

---
+++ Richie/Loops.C
@@ -0,0 +1,19 @@
+for (int i = 0; i < 10; i++)    // variable, integer named i, i is initially equal to 0, while i is less than 10, add 1 to i
+	    {
+	        printf("hello!\n");
+	    }     
+for(nt j = 0; j < 10:j++)       // three parts initialization (j=0), condition (j<10), and modification (j++)
+        {
+            printf("%d\n", j)
+        }
+int k = 0;      // initialization (k=0)
+while(k<10)     // condition (k<10)
+    {
+        printf("%d\n", k);
+        k++;
+    }
+while (true)    // this is always true, so it will run forever
+    {
+        printf("hello!\n")
+    }
+}
