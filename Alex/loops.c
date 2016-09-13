15179abf6ddefc8cd736ac3f94d74d91160241e0
SansTheSkeleton1 <laugen.alex@centennialschool.net>
2016-09-13 08:58:11 -0600

    Update loops.c

--- Alex/loops.c
+++ Alex/loops.c
@@ -1,1 +1,18 @@
+	for (int + 0; i < 10; i++)
+	{
+	    printf("hello!\n");
+For (int j = 0; j < 10;j++)
+	{
+	    printf("%d\n", j);
+	}
+int k = 0;
+while (k < 10)
+	    {
+    printf("%d\n",k);    
+     k++;
+    }
+     while (true)
+	  {      
+	   printf("hello!\n");
+	    }
 
