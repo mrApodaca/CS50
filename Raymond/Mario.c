1618d55e02dcbfeece22c5f6abed2e3080fab8fb
JamesRMendoza <mendoza.james@centennialschool.net>
2016-09-22 09:06:14 -0600

    Create Mario.c

---
+++ Raymond/Mario.c
@@ -0,0 +1,11 @@
+# include <cs50.h>
+# include <stdio.h>
+
+int main(void)
+for(int b = 0; b < hight; b++)
+{
+    printf("print spaces");
+    int n = Getint()
+    int b = n+2#;
+    printf("# %i\n",b);
+}
