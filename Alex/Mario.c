8a9afd97e01cecbbf822ac85b05f4d2818b66e69
SansTheSkeleton1 <laugen.alex@centennialschool.net>
2016-09-22 09:04:49 -0600

    Create Mario.c

---
+++ Alex/Mario.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+   {  
+   
+    printf("how tall do you want the half pyramid to be between 0-23?:");
+      
+      int n = GetInt();
+          
+          
+          for(int i = 0; i <n; i++) 
+                                          
+         { 
+           // print spaces
+           // print hashes
+          // print new line
+          //code outside
+           }
+   }
+   
