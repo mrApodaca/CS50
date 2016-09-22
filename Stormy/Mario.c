31e601a64c81d6c818dce0449a8660243659ddbe
StormySky2000 <chambers.stormy@centennialschool.net>
2016-09-22 09:04:35 -0600

    Create Mario.c

---
+++ Stormy/Mario.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+       
+        printf("How tall would you like the half pyramid to be between 0-23:");
+        
+           int n = GetInt();
+           
+           for (int i = 0; i < n; i++)
+           {   
+                   // pirntf spaces
+                   // printf hashes
+                   // new line
+                //code outside
+           }
+           
+         
+}
