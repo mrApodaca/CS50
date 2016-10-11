722c9804406cbdfdd12b930f24c91d1b052517e2
StormySky2000 <chambers.stormy@centennialschool.net>
2016-10-11 08:33:41 -0600

    Update volume.c

--- Stormy/volume.c
+++ Stormy/volume.c
@@ -1,17 +1,1 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <string.h>
 
-int main (void)
-
-{
-    printf("What is the radius of the circle?: \n");
-       
-    printf("What is the hieght of the Cylinder?: \n");
-     int r = GetInt();
-     int h = GetInt();
-         int ac = 3.14 * r *r ;
-         int vcy = ac * h;
-            
-    printf("The Volume of the cylinder is about %i" , vcy);
-}
