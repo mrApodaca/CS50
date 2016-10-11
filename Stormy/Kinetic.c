974d76ee818dbfec9734cad792944a0afe135f3e
StormySky2000 <chambers.stormy@centennialschool.net>
2016-10-11 08:33:12 -0600

    Update Kinetic.c

--- Stormy/Kinetic.c
+++ Stormy/Kinetic.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-#include <cs50.h> 
 
-
-int main(void)
-{
-         printf ("What is the Mass in Kg?:\n");
-         printf ( "What is the Velocity in M/sS?:\n");
-          int m = GetInt();
-          int v = GetInt();
-           
-              
-           int kinetic = 0.5 * m * v * v;
-               
-           
-         printf(" The kinetic energy is %i Joule \n " , kinetic);
-}
