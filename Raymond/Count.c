4432fa4a04675c00c96b00b4e95160347e8be4b0
JamesRMendoza <mendoza.james@centennialschool.net>
2016-10-13 09:04:46 -0600

    Create Count.c

---
+++ Raymond/Count.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main(void)
+{
+    for (int n = 16; n < 101; n+=3)  
+    {
+        printf("%d\n", n);
+    }
+    
+}
