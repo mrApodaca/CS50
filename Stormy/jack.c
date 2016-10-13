59b65454d7011e59275207d6b3876f2f94e7c2d8
StormySky2000 <chambers.stormy@centennialschool.net>
2016-10-13 09:06:24 -0600

    Create jack.c

---
+++ Stormy/jack.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+
+{
+   
+            
+           int s = 0; 
+while (s <= 332) 
+  {
+      printf(" All work and no play make Jack a dull boy %d\n", s);
+      s++;
+  }
+}
