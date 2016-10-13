797afab9415bb4449451a143c977a443440dcc65
JamesRMendoza <mendoza.james@centennialschool.net>
2016-10-13 09:04:16 -0600

    Create Jack.c

---
+++ Raymond/Jack.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main(void)
+{
+    int i = 0;
+while (i < 333)
+  {
+      printf("all work and no play make jack a dull boy\n");
+      i++;
+  }
+}
