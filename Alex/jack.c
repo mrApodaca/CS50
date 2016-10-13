5d21a56144c661adbf1401b67afdb7caa8dcc9fb
SansTheSkeleton1 <laugen.alex@centennialschool.net>
2016-10-13 09:02:52 -0600

    Create jack.c

---
+++ Alex/jack.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+int i= 0; 
+
+while (i <= 333)
+  {
+      printf("All work and no play make Jack a dull boy %d\n", i);
+      i++;
+  }
+  
+}
