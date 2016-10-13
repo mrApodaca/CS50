7989c38c324ee585fb18aa79f7a2cef9205ba170
StormySky2000 <chambers.stormy@centennialschool.net>
2016-10-13 15:52:58 -0600

    Update Shape.c

--- Stormy/Shape.c
+++ Stormy/Shape.c
@@ -1,25 +1,13 @@
-#include <stdio.h>
 #include <cs50.h>
 #include <math.h>
- 
-int main(void)
+#include <stdio.h> 
+
+int main (void)
 {
-    int i;
-    int r;
-    int h;
     
-        
-    i = 5;
-    
-   
-    for (r = 1 ; r <= i; r+= 1)
-    {
-        for (h = 1; h <= r + 2 ; h += 1)
-        {       
-            printf("##"); 
-            
-        }
- 
-        printf("\n");
-    }
+        printf("##########\n");
+        printf("########\n");
+        printf("######\n");
+        printf("###\n");
+        printf("#\n");
 }
