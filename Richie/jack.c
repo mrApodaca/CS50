b313e08df503b949a2e0d90a23596c6335aaf90e
RichieAtencio <RichieAtencio@users.noreply.github.com>
2016-10-13 09:05:28 -0600

    Create jack.c

---
+++ Richie/jack.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int j = 0;
+    
+    while(j<333)
+    {
+        printf("All work no play makes Jack a dull boy.\n");
+        j++;
+    }
+}
