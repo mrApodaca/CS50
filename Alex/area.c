9524277c93a45e44d5f0f0f072b8a616d46b064d
SansTheSkeleton1 <laugen.alex@centennialschool.net>
2016-10-13 09:05:34 -0600

    Create area.c

---
+++ Alex/area.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{      
+    printf("what's your radius?:\n");
+    int radius = GetInt();
+   
+
+
+    printf("what's your height of the circle?");  
+
+       
+     int area= (3.14 * radius * radius);
+       printf(" %i radius", area);       
+       printf(" %i area", radius);
+      }
